15c30329059795a2cb09c225c542e9d23734649d
Arkit Patel <arkitpatel3697@gmail.com>
2017-12-18 23:52:33 +0530

    Mini project of Choose your own Algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,9 +3,9 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from time import time
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
@@ -33,10 +33,15 @@ plt.show()
 # your code here!  name your classifier object clf if you want the
 # visualization code (prettyPicture) to show you the decision boundary
 
-# clf = GaussianNB()
-clf = SVC(kernel="rbf", gamma=1000, C=1)
+clf = RandomForestClassifier(criterion="entropy", min_samples_split=100, n_estimators=100000)
+t = time()
 clf.fit(features_train, labels_train)
-pred = clf.predict(features_test)
+print "training time:", round(time()-t, 3), "s"
+t = time()
+prediction = clf.predict(features_test)
+print "prediction time:", round(time()-t, 3), "s"
+accuracy = accuracy_score(labels_test, prediction)
+print accuracy
 
 try:
     prettyPicture(clf, features_test, labels_test)
